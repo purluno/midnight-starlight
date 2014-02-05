15e4eca985069a19abc16d9cf542031dfd2e5cec
Song Younghwan <purluno@gmail.com>
2014-02-06 02:59:35 +0900

    Logback groovy 설정파일 설명 작성

--- src/main/webapp/WEB-INF/classes/logback.groovy
+++ src/main/webapp/WEB-INF/classes/logback.groovy
@@ -3,6 +3,9 @@ import ch.qos.logback.core.ConsoleAppender
 
 import static ch.qos.logback.classic.Level.*
 
+// Logback의 그루비 설정파일은 class로 컴파일하지 않고 groovy 파일을 그대로 읽으므로
+// classes 폴더에 소스코드 그대로 위치해야 한다.
+
 appender("STDOUT", ConsoleAppender) {
   encoder(PatternLayoutEncoder) {
 	pattern = "%d{HH:mm:ss.SSS} [%.5thread] %.-1level %logger{36} - %msg%n%xEx"
