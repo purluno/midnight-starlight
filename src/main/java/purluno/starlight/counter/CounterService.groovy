2e0ee0fc38b2efc5a8c9454f1e61e449312c04bf
Song Younghwan <purluno@gmail.com>
2014-02-27 21:02:10 +0900

    [Finishes #65271980] 전체 방문자 수 표시 기능 구현

---
+++ src/main/java/purluno/starlight/counter/CounterService.groovy
@@ -0,0 +1,45 @@
+package purluno.starlight.counter
+
+import javax.annotation.Resource
+
+import org.hibernate.SessionFactory
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+interface CounterService {
+	long getTotalHits()
+	
+	void hit()
+}
+
+@Service
+@Transactional
+class DefaultCounterService implements CounterService {
+	@Resource
+	private SessionFactory sessionFactory
+	
+	@Override
+	long getTotalHits() {
+		def session = sessionFactory.currentSession
+		def tc = session.get(TotalCounter, 1L) as TotalCounter
+		if (tc == null) {
+			tc = new TotalCounter()
+			tc.value = 0
+			session.save(tc)
+		}
+		tc.value
+	}
+
+	@Override
+	void hit() {
+		def session = sessionFactory.currentSession
+		def hql = "update TotalCounter set value = value + 1"
+		def n = session.createQuery(hql).executeUpdate()
+		if (n != 1) {
+			session.createQuery("delete from TotalCounter").executeUpdate()
+			def tc = new TotalCounter()
+			tc.value = 1
+			session.save(tc)
+		}
+	}
+}
