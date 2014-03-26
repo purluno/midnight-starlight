e1195fc9ae2eb169ae34d3e27f8a861d888ada91
Song Younghwan <purluno@gmail.com>
2014-03-27 01:07:09 +0900

    정책 변경에 따른 주석 삭제

--- src/main/resources/logback.default.groovy
+++ src/main/resources/logback.default.groovy
@@ -3,8 +3,6 @@ import ch.qos.logback.core.ConsoleAppender
 
 import static ch.qos.logback.classic.Level.*
 
-// Logback의 그루비 설정파일은 class로 컴파일하지 않고 groovy 파일을 그대로 읽으므로
-// classes 폴더에 소스코드 그대로 위치해야 한다.
 
 appender("STDOUT", ConsoleAppender) {
   encoder(PatternLayoutEncoder) {
