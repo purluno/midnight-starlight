f270476947aed88f4ac3ff6f30163bf06bf5a1a8
Song Younghwan <purluno@gmail.com>
2014-03-27 00:58:56 +0900

    settings.groovy를 통한 프로그램 설정 기능 추가

--- src/main/java/purluno/starlight/AppConfig.groovy
+++ src/main/java/purluno/starlight/AppConfig.groovy
@@ -1,10 +1,13 @@
 package purluno.starlight
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
 import org.springframework.stereotype.Controller
+import org.springframework.util.ResourceUtils
 
 import twitter4j.Twitter
 import twitter4j.TwitterFactory
@@ -18,8 +21,23 @@ import twitter4j.TwitterFactory
 @ComponentScan(basePackageClasses = AppConfig, excludeFilters = @ComponentScan.Filter(Controller))
 @Import([AppConfigHibernate, AppConfigShiro])
 class AppConfig {
+	static Logger logger = LoggerFactory.getLogger(AppConfig)
+
 	@Bean
 	Twitter defaultTwitter() {
 		TwitterFactory.singleton
+	}
+
+	@Bean
+	ConfigObject settings() {
+		try {
+			def url = ResourceUtils.getURL("classpath:settings.groovy")
+			return new ConfigSlurper().parse(url)
+		} catch (FileNotFoundException e) {
+			logger.warn("settings.groovy not found")
+		} catch (IOException e) {
+			logger.warn("cannot read settings.groovy")
+		}
+		new ConfigObject()
 	}
 }
---
+++ src/main/resources/settings.default.groovy
@@ -0,0 +1,2 @@
+// 주인 권한을 가지는 principal 지정
+owner = 'username@origin'
